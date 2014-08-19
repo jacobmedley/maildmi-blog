87ddf467ecd965761f4fc4eed1b5bd4f392224dc
jacobmedley <jacob@medleyindesign.com>
2014-08-18 22:52:21 -0400

    v1.1 commit
    v1.1 commit

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 		</div>
 		<hr />
 		<div class="row">
-			<div class="col-lg-8">
+			<div class="col-lg-8"> 
 				<div class="row">
 					<div class="col-lg-12">
 										<?php /* Primary navigation */
